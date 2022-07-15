79b389df119552a79d69437d0e391d821829040b
ravi04ez <ezekie.em@gmail.com>
2022-07-16 00:59:23 +0530

    Welcm Msg for Basic Core Program

---
+++ BasicCorePrograms/BasicCoreProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicCoreProgram
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t#################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO BASIC CORE PROGRAMS");
+            Console.WriteLine("\t\t\t\t#################################################");
+            Console.ReadKey();
+        }
+    }
+}
