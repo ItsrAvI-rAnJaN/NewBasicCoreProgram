92675730c187189cd7f5dd21f4b2757c949e5cfb
ravi04ez <ezekie.em@gmail.com>
2022-07-18 13:20:11 +0530

    Added Leap Year

---
+++ BasicCorePrograms/BasicCoreProgram/LeapYear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        
+        
+       public void CheckLeap()
+        {
+            Console.Write("Enter The Four Digit Number of Years: ");
+           int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year>999 && year < 9999)
+            {
+                if(year%400==0&& year%100==0 || year % 4 == 0)
+                {
+                    Console.WriteLine("The Entered Year is a leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("The Entered Year is not Leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Input");
+            }
+        }
+    }
+}
--- BasicCorePrograms/BasicCoreProgram/Program.cs
+++ BasicCorePrograms/BasicCoreProgram/Program.cs
@@ -9,8 +9,26 @@ namespace BasicCoreProgram
             Console.WriteLine("\t\t\t\t#################################################");
             Console.WriteLine("\t\t\t\t\tWELCOME TO BASIC CORE PROGRAMS");
             Console.WriteLine("\t\t\t\t#################################################");
-            FlipCoin objFlipcoin=new FlipCoin();
-            objFlipcoin.CheckHeadTail();
+            Console.WriteLine("0.Exit");
+            Console.WriteLine("1.FlipCoin & print Percentage of Heads vs Tails");
+            Console.WriteLine("2.Program to check Leap Year");
+            Console.Write("Enter The Choice of Question: ");
+            int Choice=Convert.ToInt32(Console.ReadLine());
+            switch (Choice)
+            {
+                case 1:
+                     FlipCoin objFlipcoin=new FlipCoin();
+                     objFlipcoin.CheckHeadTail();
+                     break;
+                case 2:
+                    LeapYear objLeapYear =new LeapYear();
+                    objLeapYear.CheckLeap();
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Question Number");
+                    break;
+            }
+            
             Console.ReadKey();
         }
     }
