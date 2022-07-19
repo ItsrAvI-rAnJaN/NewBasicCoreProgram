9bb70e4c4b9ce8d2278639ef26179d253b78b894
ravi04ez <ezekie.em@gmail.com>
2022-07-19 14:19:45 +0530

    Added prime factor

---
+++ BasicCorePrograms/BasicCoreProgram/Factors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class Factors
+    {
+        public void PrimeFactorization()
+        {
+            Console.Write("Enter a Number to Find Prime Factors : ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Prime Factors of {0} = ", Num);
+
+            //Taking While Loop for The Number is Divisible by 2 We are Dividing it..
+            while (Num % 2 == 0)
+            {
+                Console.Write(2 + " ");
+                Num /= 2;
+            }
+
+            //This  is For All The Odd Numbers Greater Than 2...
+            for (int i = 3; i <= Math.Sqrt(Num); i += 2)
+            {
+                while (Num % i == 0)
+                {
+                    Console.Write(i + " ");
+                    Num /= i;
+                }
+            }
+            if (Num > 2)
+            {
+                Console.Write(Num);
+            }
+
+        }
+    }
+}
--- BasicCorePrograms/BasicCoreProgram/Program.cs
+++ BasicCorePrograms/BasicCoreProgram/Program.cs
@@ -14,7 +14,8 @@ namespace BasicCoreProgram
             Console.WriteLine("2.Program to check Leap Year");
             Console.WriteLine("3.Table Of Two up to 31");
             Console.WriteLine("4.Print The Nth Harmonic Number");
-            Console.Write("Enter The Choice of Question: ");
+            Console.WriteLine("5.Calculate the Factor of N Number");
+            Console.Write("\nEnter The Choice of Question: ");
             int Choice=Convert.ToInt32(Console.ReadLine());
             switch (Choice)
             {
@@ -33,6 +34,10 @@ namespace BasicCoreProgram
                 case 4:
                     HarmonicNumber objharmonicno = new HarmonicNumber();
                     objharmonicno.harmonicVal();
+                    break;
+                case 5:
+                    Factors objfactor = new Factors();
+                    objfactor.PrimeFactorization();
                     break;
 
                 default:
